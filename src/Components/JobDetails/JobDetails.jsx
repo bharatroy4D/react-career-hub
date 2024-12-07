1610b2b383dff4bba61c6a00a7941f7ec4fa3bbf
Mr. Bharat Roy <bharatroy1005@gmail.comgidjfclsgir>
2024-12-07 18:35:53 +0600

    job details of title asset added

--- src/Components/JobDetails/JobDetails.jsx
+++ src/Components/JobDetails/JobDetails.jsx
@@ -8,10 +8,19 @@ const JobDetails = () => {
     console.log(job);
     return (
         <div>
-            <h2>job Details of : {id}</h2>
+
             <div className="grid gap-4 md:grid-cols-4">
                 <div className="border md:col-span-3">
-                    <h2 className="text-4xl">Details comings Here</h2>
+                    <h2><span className="font-bold text-xl">Job Descriptions</span> : {job.job_description}</h2>
+                    <h2><span className="font-bold text-xl">Job Responsibility</span> : {job.job_responsibility} </h2>
+                    <div>
+                        <p className="font-bold text-xl">Educational Requirement :</p>
+                        <p>{job.educational_requirements}</p>
+                    </div>
+                    <div>
+                        <p className="font-bold text-xl">Experiences :</p>
+                        <p>{job.experiences}</p>
+                    </div>
                 </div>
                 <div className="border">
                     <h2 className="text-2xl">Site Think</h2>
